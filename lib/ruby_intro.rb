e734559ea7d6bcceffe627f535abf35f1f87f0a5
EC2 Default User <ec2-user@ip-172-31-20-149.us-east-2.compute.internal>
2021-09-12 19:05:39 +0000

    finished part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,14 +4,46 @@
 
 def sum arr
   # YOUR CODE HERE
+  x = 0
+  arr.each {|val| x += val}
+  return x
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.size == 0
+    return 0
+  elsif arr.size == 1
+    return arr[0]
+  else
+    mv1 = arr[0]
+    mv2 = arr[1]
+    arr[2..].each do |val|
+      if val > [mv1,mv2].min
+        if mv1 < mv2
+          mv1 = val
+        else
+          mv2 = val
+        end
+        
+      end      
+    end
+    return mv1+mv2
+  end
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  arr.each_with_index do |val1,index1|
+    arr.each_with_index do |val2,index2|
+      if index1 != index2
+        if val1 + val2 == n
+          return true
+        end
+      end
+    end
+  end
+  return false
 end
 
 # Part 2

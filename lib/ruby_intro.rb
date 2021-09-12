d0bc78aeba68b1543b57b23113419db8777dbb8e
Jared Jones <jared.jones280@gmail.com>
2021-09-12 20:56:39 +0000

    Finished parts 1-3

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -50,18 +50,44 @@ end
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  if s.match (/^[b-df-hj-np-tv-z]/i)
+    return true
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if s.match(/(^[01]*[1]+[01]*(00)$|^[0]$)/)
+    return true
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+  def initialize(is,pr)
+    raise ArgumentError.new(
+      "ISBN must not be empty"
+      )if (is == "")
+    raise ArgumentError.new(
+      "Price must be > $0.00"
+      )if pr <= 0
+    @price = pr
+    @isbn = is
+  end
+  
+  attr_accessor :price, :isbn
+  
+  def price_as_string
+    return "$#{'%.2f'%@price}"
+  end
 end
